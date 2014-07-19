41bd196baffe3c7c3b2b6016593c4fb741f60d67
AkihiroKOBAYASHI <sun.moon.bow.reverse7@gmail.com>
2014-07-19 13:27:35 +0900

    add the movement of car and camera

---
+++ thesis2014/Assets/CarMovement.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+private var spd:float = 0.05;
+
+function Start () {
+
+}
+
+function Update () {
+
+	if (Input.GetKey("right")) {
+		transform.position.x += spd;
+	}
+	else if (Input.GetKey("left")) {
+		transform.position.x -= spd;
+	}
+	
+	
+	if (Input.GetKey("up")) {
+		transform.position.z += spd;
+	}
+	else if (Input.GetKey("down")) {
+		transform.position.z -= spd;
+	}
+		
+}
