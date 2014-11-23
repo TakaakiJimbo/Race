f9821c85e38965add0f9848badb2b7a749feaa83
AkihiroKOBAYASHI <sun.moon.bow.reverse7@gmail.com>
2014-11-23 21:22:42 +0900

    repair the bear

--- thesis2014/Assets/MyGame/OriginalPrefabSetting/RunningBear/FollowAgent.cs
+++ thesis2014/Assets/MyGame/OriginalPrefabSetting/RunningBear/FollowAgent.cs
@@ -2,26 +2,26 @@ using UnityEngine;
 using System.Collections;
 
 public class FollowAgent : MonoBehaviour {
-
+	
 //	public GameObject			particle;
 	protected NavMeshAgent		agent;
 	protected Animator			animator;
-
+	
 	protected Locomotion locomotion;
-	protected Object particleClone;
-
-
+//	protected Object particleClone;
+	
+	
 	// Use this for initialization
 	void Start () {
 		agent = GetComponent<NavMeshAgent>();
 		agent.updateRotation = false;
-
+		
 		animator = GetComponent<Animator>();
 		locomotion = new Locomotion(animator);
-
+		
 //		particleClone = null;
 	}
-
+	
 	protected void SetDestination()
 	{
 //		// Construct a ray from the current mouse coordinates
@@ -34,16 +34,16 @@ public class FollowAgent : MonoBehaviour {
 //				GameObject.Destroy(particleClone);
 //				particleClone = null;
 //			}
-//
+//			
 //			// Create a particle if hit
 //			Quaternion q = new Quaternion();
 //			q.SetLookRotation(hit.normal, Vector3.forward);
 //			particleClone = Instantiate(particle, hit.point, q);
-//
+//			
 //			agent.destination = hit.point;
 //		}
 	}
-
+	
 	protected void SetupAgentLocomotion()
 	{
 		if (AgentDone())
@@ -58,25 +58,25 @@ public class FollowAgent : MonoBehaviour {
 		else
 		{
 			float speed = agent.desiredVelocity.magnitude;
-
+			
 			Vector3 velocity = Quaternion.Inverse(transform.rotation) * agent.desiredVelocity;
-
+			
 			float angle = Mathf.Atan2(velocity.x, velocity.z) * 180.0f / 3.14159f;
-
+			
 			locomotion.Do(speed, angle);
 		}
 	}
-
-    void OnAnimatorMove()
-    {
-        agent.velocity = animator.deltaPosition / Time.deltaTime;
+	
+	void OnAnimatorMove()
+	{
+		agent.velocity = animator.deltaPosition / Time.deltaTime;
 		transform.rotation = animator.rootRotation;
-    }
+	}
 
 	void OnTriggerStay(Collider other) 
 	{
-//		if ( this.enabled == false ) return;
-		Vector3 dir = this.transform.position + other.transform.position;
+		if ( this.enabled == false ) return;
+		Vector3 dir = this.transform.position - other.transform.position;
 		Vector3 pos = this.transform.position + dir * 0.5f;
 		agent.destination = pos;
 	}
@@ -85,18 +85,18 @@ public class FollowAgent : MonoBehaviour {
 	{
 		return !agent.pathPending && AgentStopping();
 	}
-
+	
 	protected bool AgentStopping()
 	{
 		return agent.remainingDistance <= agent.stoppingDistance;
 	}
-
+	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetButtonDown ("Fire1")) 
-			SetDestination();
-
+//		if (Input.GetButtonDown ("Fire1")) 
+//			SetDestination();
+		
 		SetupAgentLocomotion();
 	}
 }
