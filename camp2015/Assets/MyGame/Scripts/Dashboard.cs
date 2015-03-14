4d01c1cfcb2d04852359501a3b8aab29af4e82e6
AkihiroKOBAYASHI <sun.moon.bow.reverse7@gmail.com>
2015-03-14 15:36:00 +0900

    add the collision @camp2015Demo

---
+++ camp2015/Assets/MyGame/Scripts/Dashboard.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dashboard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Car 
+	void OnTriggerEnter(Collider other)
+	{
+		CheckColliderTag(other);
+	}
+
+	void CheckColliderTag(Collider other)
+	{
+		Debug.Log (other.gameObject.tag);
+	}
+
+}
