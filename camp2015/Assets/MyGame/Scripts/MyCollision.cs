91a9543638f96f5f270a71d7c632c6f8155d06fa
AkihiroKOBAYASHI <sun.moon.bow.reverse7@gmail.com>
2015-03-15 15:55:33 +0900

    repair the program "MyCollision.cs"

---
+++ camp2015/Assets/MyGame/Scripts/MyCollision.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Car 
+	void OnTriggerEnter(Collider other) {
+		switch (CheckColliderTag (other.gameObject.tag)) {
+		case 1 :
+			break;
+		default :
+			break;
+		}
+	}
+
+	int CheckColliderTag(string tag) {
+		switch (tag) {
+			case "Dashboard" : 
+				return 1;
+				break;
+			default :
+				return 0;
+				break;
+		}
+	}
+
+}
