3057e906bc6c977a0d92e8cfe71ebabb154f7710
AkihiroKOBAYASHI <sun.moon.bow.reverse7@gmail.com>
2015-03-16 21:58:38 +0900

    add the Bomb

--- camp2015/Assets/MyGame/Scripts/MyCollision.cs
+++ camp2015/Assets/MyGame/Scripts/MyCollision.cs
@@ -8,11 +8,6 @@ namespace UnitySampleAssets.Vehicles.Car  {
 		private float[] initialization = new float[]{CarController.maxTorque, CarController.minTorque, CarController.maxSpeed};
 
 
-		void Update() {
-			transform.Rotate(new Vector3(0, 720, 0) * Time.deltaTime);
-		}
-
-
 		void OnTriggerEnter(Collider other) {
 			switch (CheckColliderTag (other.gameObject.tag)) {
 			case 1 :
@@ -20,6 +15,9 @@ namespace UnitySampleAssets.Vehicles.Car  {
 				break;
 			case 4 :
 				Banana(other);
+				break;
+			case 5 :
+				Bomb(other);
 				break;
 			default :
 				break;
@@ -60,6 +58,9 @@ namespace UnitySampleAssets.Vehicles.Car  {
 			case "Banana" : 
 				return 4;
 				break;
+			case "Bomb" : 
+				return 5;
+				break;
 			default :
 				return 0;
 				break;
@@ -84,7 +85,21 @@ namespace UnitySampleAssets.Vehicles.Car  {
 			BananaRotate ();
 		}
 
+
 		void BananaRotate() {
+			iTween.RotateTo(gameObject, iTween.Hash("y", 1080, "time", 2.0f));
+		}
+
+
+		void Bomb(Collider other) {
+			Destroy (other.gameObject);
+			BombFly ();
+		}
+
+
+		void BombFly() {
+			iTween.MoveTo(gameObject, iTween.Hash("y", 20, "time", 1.5f));
+			iTween.RotateTo(gameObject, iTween.Hash("x", 1440, "time", 5.5f));
 		}
 
 
