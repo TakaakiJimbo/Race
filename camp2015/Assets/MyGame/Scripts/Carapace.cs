088e726110295b115255ae41c7bab6abcc5ebf99
AkihiroKOBAYASHI <sun.moon.bow.reverse7@gmail.com>
2015-03-18 15:49:17 +0900

    add the Carapace

--- camp2015/Assets/MyGame/Scripts/Carapace.cs
+++ camp2015/Assets/MyGame/Scripts/Carapace.cs
@@ -3,14 +3,22 @@ using System.Collections;
 
 public class Carapace : MonoBehaviour {
 
-	private float movePower = 800.0f;
-
-	// Use this for initialization
-	void Start () {
-	}
+	private float movePower = 200.0f;
+	private Vector3 cashVelocity;
+	private float maxSpeed = 20;
 	
 	// Update is called once per frame
 	void Update () {
+		rigidbody.AddForce (transform.forward * movePower * Time.deltaTime);
+		if (rigidbody.velocity.z * cashVelocity.z < 0 ||rigidbody.velocity.x * cashVelocity.x < 0 ) {
+			transform.LookAt(transform.position + rigidbody.velocity);
+		}
+		if (rigidbody.velocity.y * cashVelocity.y < 0) {
+			rigidbody.velocity = new Vector3(rigidbody.velocity.x, 0, rigidbody.velocity.z);
+		}
+		cashVelocity = rigidbody.velocity;
+		if (rigidbody.velocity.magnitude > maxSpeed) {
+			rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
+		}
 	}
-
 }
