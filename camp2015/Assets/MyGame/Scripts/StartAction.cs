478e4664d3797442b281a40ad7941aa1e7190f69
AkihiroKOBAYASHI <sun.moon.bow.reverse7@gmail.com>
2015-04-04 21:09:21 +0900

    add the start function

---
+++ camp2015/Assets/MyGame/Scripts/StartAction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartAction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("DeleteCover", 3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void DeleteCover(){
+		Destroy (gameObject.transform.FindChild("StartCover").gameObject);
+	}
+}
