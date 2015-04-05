f1b8c4b54646a336bd3afd8e0d0b8bb91f6a70e3
AkihiroKOBAYASHI <sun.moon.bow.reverse7@gmail.com>
2015-04-05 10:24:02 +0900

    add the counttimer for start

--- camp2015/Assets/MyGame/Scripts/StartAction.cs
+++ camp2015/Assets/MyGame/Scripts/StartAction.cs
@@ -1,19 +1,34 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class StartAction : MonoBehaviour {
+	public  GameObject startcount;
+	private Text       startcountvalue;
+	public  float      timer;
 
 	// Use this for initialization
 	void Start () {
-		Invoke ("DeleteCover", 3);
+		startcount = GameObject.Find("StartCount").gameObject;
+		startcountvalue = startcount.GetComponent<Text>();
+		timer = 3.0f;
 	}
 	
-	// Update is called once per frame
 	void Update () {
+		timer -= Time.deltaTime;
+		if (timer <= 0.0f)
+		{
+			Destroy(startcount);
+			Destroy(gameObject);
+		}
+		else if (timer <= 1.0f)
+		{
+			startcountvalue.text = "1" ;
+		}
+		else if (timer <= 2.0f)
+		{
+			startcountvalue.text = "2" ;
+		}
+	}
 	
-	}
-
-	void DeleteCover(){
-		Destroy (gameObject.transform.FindChild("StartCover").gameObject);
-	}
 }
