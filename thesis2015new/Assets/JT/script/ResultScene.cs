cd888dc3f7e753216590dcb6b54eeec6b3d7b23a
神保貴昭 <jt0820cgsk@gmail.com>
2015-09-08 14:51:51 +0900

    Add title/select

---
+++ thesis2015new/Assets/JT/script/ResultScene.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;  
+
+public class ResultScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		winP(ButtonA.playerNum);
+	}
+
+
+
+public void winP(int number){
+	
+	switch (number) {
+	case 0:
+			this.GetComponent<Text>().text = "PLAYER 1・2 WIN!";
+		break;
+		
+	case 1:
+			this.GetComponent<Text>().text = "PLAYER 3・4 WIN!";
+		break;
+		
+	default:
+		break;
+		}
+	}
+
+}
